c1b0114ff9089fed2c7b15089becf07207693ee0
Lwolwo <zuihouyiye0704@126.com>
2019-05-25 18:26:55 +0800

    feat: 完成商店部分UI和功能

--- pages/data/desireData.js
+++ pages/data/desireData.js
@@ -1,43 +1,48 @@
-var desireData = [
-  {
-    desireid: 0,
-    title: '打一局王者',
-    grade: 0,
-    get: 0
-  },
-  {
-    desireid: 1,
-    title: '八合里走起！！！',
-    grade: 3,
-    get: 0
-  },
-  {
-    desireid: 2,
-    title: '买一支口红',
-    grade: 2,
-    get: 0
-  },
-  {
-    desireid: 3,
-    title: '买皮肤',
-    grade: 3,
-    get: 0
-  },
-  {
-    desireid: 4,
-    title: '补觉一天！！！',
-    grade: 3,
-    get: 0
-  },
-  {
-    desireid: 5,
-    title: '桂庙吃大餐',
-    grade: 1,
-    get: 0
-  }
+var desireData = [{
+        desireid: 0,
+        title: '打一局王者',
+        grade: 0,
+        get: 0,
+        allGet: 1
+    },
+    {
+        desireid: 1,
+        title: '八合里走起！！！',
+        grade: 3,
+        get: 0,
+        allGet: 2
+    },
+    {
+        desireid: 2,
+        title: '买一支口红',
+        grade: 2,
+        get: 2,
+        allGet: -1
+    },
+    {
+        desireid: 3,
+        title: '买皮肤',
+        grade: 3,
+        get: 1,
+        allGet: -1
+    },
+    {
+        desireid: 4,
+        title: '补觉一天！！！',
+        grade: 3,
+        get: 7,
+        allGet: -1
+    },
+    {
+        desireid: 5,
+        title: '桂庙吃大餐',
+        grade: 1,
+        get: 0,
+        allGet: 10
+    }
 
 ]
 
 module.exports = {
-  desireData: desireData
+    desireData: desireData
 }
--- pages/index/store/store.js
+++ pages/index/store/store.js
@@ -3,50 +3,215 @@ const userData = require('../../data/userData.js')
 
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    price: [10, 20, 30, 40]
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        price: [2, 4, 9, 16],
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    this.setData({
-      desireData: desireData.desireData,
-      userData: userData.userData
-    })
-  },
-  buyDesire: function (e) {
-    var desireid = e.currentTarget.dataset.desireid
-    var self = this
-    var user = this.data.userData
-    var price = this.data.price[this.data.desireData[desireid].grade]
-    wx.showModal({
-      title: '确认',
-      content: '确定使用￥' + self.data.price[self.data.desireData[desireid].grade] + ' 购买 \"' + self.data.desireData[desireid].title + '\" 这个欲望吗？',
-      success(res) {
-        if (res.confirm) {
-          if (user.money >= price) {
-            self.data.desireData[desireid].get = !self.data.desireData[desireid].get
-            user.money -= price
-            self.setData({
-              desireData: self.data.desireData,
-              userData: user
-            })
-          }
-          else {
+        taskname: '',
+        taskcount: '',
+
+        difficulty: ['请选择难度', '简单', '普通', '中等', '困难'],
+        index: 0
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        this.setData({
+            desireData: desireData.desireData,
+            userData: userData.userData
+        })
+    },
+
+    buyDesire: function(e) {
+        var desireid = e.currentTarget.dataset.desireid
+        var self = this
+        var user = this.data.userData
+        var desireArray = this.data.desireData
+
+        let index = self.findItem(desireArray, desireid)
+        let item = self.data.desireData[index]
+        let price = this.data.price[item.grade]
+
+
+        wx.showModal({
+            title: '确认',
+            content: '确定使用￥' + price + ' 购买 \"' + item.title + '\" 这个欲望吗？',
+
+            success(res) {
+                if (res.confirm) {
+                    if (user.money >= price) {
+
+
+                        item.get++;
+                        if (item.get >= item.allGet && item.allGet != -1) {
+                            if (index > -1)
+                                desireArray.splice(index, 1);
+
+
+                        }
+
+
+                        user.money -= price
+
+
+                        self.setData({
+                            desireData: desireArray,
+                            userData: user
+                        })
+
+                    } else {
+                        wx.showToast({
+                            title: '￥余额不足，要继续努力才可以哦！',
+                            icon: 'none',
+                            duration: 1500,
+                            mask: true
+                        })
+                    }
+
+                }
+            }
+        })
+
+    },
+
+    clickButton() {
+        var self = this
+        var user = this.data.userData
+        var desireArray = this.data.desireData
+
+        wx.showActionSheet({
+            itemList: ['添加', '排序'],
+            success(res) {
+                /* 初始化要添加的商品信息 */
+                if (res.tapIndex == 0) {
+                    self.setData({
+                        showModal: true,
+                        index: 0,
+                        taskname: '',
+                        taskcount: '',
+
+                    })
+
+
+                }
+
+                if (res.tapIndex == 1) {
+                    desireArray.sort(function(a, b) {
+                        if (a.grade < b.grade) {
+                            return -1;
+                        } else {
+                            return 1;
+                        }
+                    })
+
+                    self.setData({
+                        desireData: desireArray,
+
+                    })
+                }
+
+
+
+
+            }
+        })
+    },
+
+    close_mask: function() {
+        this.setData({
+            showModal: false
+        })
+    },
+
+    bindPickerChange: function (e) {
+        this.setData({
+            index: e.detail.value
+        })
+    },
+
+    addDesire() {
+        var self = this
+        let taskname = self.data.taskname;
+        let taskcount = self.data.taskcount;
+        let grade = self.data.index - 1;
+
+        console.log("taskname", taskname)
+        console.log("taskcount", taskcount)
+        console.log("grade", grade)
+
+        if (taskname.length == 0) {
             wx.showToast({
-              title: '￥余额不足，要继续努力才可以哦！',
-              icon: 'none',
-              duration: 1500,
-              mask: true
+                title: '请输入商品名',
+                duration: 2000,
+                mask: true,
+                icon: 'none'
             })
-          }
+            return
         }
-      }
-    })
-  }
+        else if (grade === -1) {
+            wx.showToast({
+                title: '请选择难度',
+                duration: 2000,
+                mask: true,
+                icon: 'none'
+            })
+            return
+        }
+
+        if (taskcount.length == 0) {
+            taskcount = -1;
+        } else {
+            taskcount = Number(taskcount);
+        }
+
+        var newdata = {
+            desireid: self.data.desireData.length,
+            title: taskname,
+            grade: grade,
+            get: 0,
+            allGet: taskcount
+        }
+
+        console.log(newdata)
+
+        self.data.desireData.push(newdata)
+
+
+        self.setData({
+            desireData: self.data.desireData,
+            showModal: false
+        })
+
+        
+
+
+    },
+
+    getTaskName(e) {
+        var val = e.detail.value
+        this.setData({
+            taskname: val
+        })
+    },
+
+    getTaskCount(e) {
+        var val = e.detail.value
+        this.setData({
+            taskcount: val
+        })
+    },
+
+    findItem(array, id) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i].desireid == id)
+                return i;
+        }
+        return -1;
+    }
+
+
 })
