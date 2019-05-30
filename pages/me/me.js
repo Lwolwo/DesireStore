e0fe34ec97465650bc88362572cf84003b65c715
Lwolwo <zuihouyiye0704@126.com>
2019-05-30 17:34:44 +0800

    feat: 增加显示具体经验值功能

--- pages/me/me.js
+++ pages/me/me.js
@@ -26,14 +26,6 @@ Page({
         }
     },
 
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function() {
-
-    },
-
     /**
      * 生命周期函数--监听页面显示
      */
@@ -42,39 +34,19 @@ Page({
             userData: wx.getStorageSync('userData')
         })
     },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function() {
-
+    
+    getExp() {
+        var needExp = getApp().globalData.levelExp;
+        needExp = needExp[this.data.userData.level + 1]
+        this.setData({
+            showModal: true,
+            needExp: needExp
+        })
+    },
+    close_mask: function () {
+        this.setData({
+            showModal: false
+        })
     },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function() {
-
-    }
 })
